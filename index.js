3a8f581317b8bcf05f2053b95fe19e223b0fc67f
1hk-star <1hk_star@naver.com>
2021-01-07 20:46:41 +0900

    0107

--- index.js
+++ index.js
@@ -2,13 +2,36 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://lee:1234@react-cluster.usg0j.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://lee:1234@react-cluster.usg0j.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(() => console.log('MongoDB connected..'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+  //회원 가입할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣는다.
+
+  const user = new User(req.body)
+  user.save((err, userInfo) =>{
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
